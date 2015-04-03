f20514bee277111a5478deba50f262e8a17587b4
Allan Xiao <allan.xiao@gmail.com>
2015-04-03 12:42:14 -0400

    -change sql schema for tbl_items.Name to 100 characters -add Provider for Dice

---
+++ Watcher.Provider.Dice/DiceSource.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Watcher.Extensions;
+
+namespace Watcher.Provider.Dice
+{
+    class DiceSource : AbstractSource
+    {
+        
+        public DiceSource(string name)
+            : base(name, DiceProvider.PROVIDER)
+        {
+
+        }
+
+
+        public override string GetDisplayName()
+        {
+            return SourceName;
+        }
+    }
+}
